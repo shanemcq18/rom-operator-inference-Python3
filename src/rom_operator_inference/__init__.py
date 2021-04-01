bfb487e1b06454994f4c1d256e86273176a18285
Shane <shanemcq18@gmail.com>
2021-04-01 17:10:08 -0500

    increment version, add lstsq to namespace

--- src/rom_operator_inference/__init__.py
+++ src/rom_operator_inference/__init__.py
@@ -7,7 +7,7 @@ GitHub:
     https://github.com/Willcox-Research-Group/rom-operator-inference-Python3
 """
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 from ._core import *
-from . import utils, pre, post
+from . import lstsq, pre, post, utils
