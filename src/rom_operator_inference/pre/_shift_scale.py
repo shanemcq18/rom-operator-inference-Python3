1a259213970ac4e4d9885ae1feeecbdf32eb958f
Shane <shanemcq18@gmail.com>
2021-06-09 16:59:31 -0500

    new SnapshotTransformer

--- src/rom_operator_inference/pre/_shift_scale.py
+++ src/rom_operator_inference/pre/_shift_scale.py
@@ -1,9 +1,10 @@
-# pre/_shiftscale.py
+# pre/_shift_scale.py
 """Tools for preprocessing data."""
 
 __all__ = [
             "shift",
             "scale",
+            "SnapshotTransformer"
           ]
 
 import numpy as np
@@ -17,7 +18,6 @@ def shift(X, shift_by=None):
     ----------
     X : (n,k) ndarray
         A matrix of k snapshots. Each column is a single snapshot.
-
     shift_by : (n,) or (n,1) ndarray
         A vector that is the same size as a single snapshot. If None,
         set to the mean of the columns of X.
@@ -26,13 +26,10 @@ def shift(X, shift_by=None):
     -------
     Xshifted : (n,k) ndarray
         The matrix such that Xshifted[:,j] = X[:,j] - shift_by for j=0,...,k-1.
-
     xbar : (n,) ndarray
         The shift factor. Since this is a one-dimensional array, it must be
         reshaped to be applied to a matrix: Xshifted + xbar.reshape((-1,1)).
         Only returned if shift_by=None.
-
-    For shift_by=None, only Xshifted is returned.
 
     Examples
     --------
@@ -70,10 +67,8 @@ def scale(X, scale_to, scale_from=None):
     ----------
     X : (n,k) ndarray
         A matrix of k snapshots to be scaled. Each column is a single snapshot.
-
     scale_to : (2,) tuple
         The desired minimum and maximum of the scaled data.
-
     scale_from : (2,) tuple
         The minimum and maximum of the snapshot data. If None, learn the
         scaling from X: scale_from[0] = min(X); scale_from[1] = max(X).
@@ -82,17 +77,13 @@ def scale(X, scale_to, scale_from=None):
     -------
     Xscaled : (n,k) ndarray
         The scaled snapshot matrix.
-
     scaled_to : (2,) tuple
         The bounds that the snapshot matrix was scaled to, i.e.,
         scaled_to[0] = min(Xscaled); scaled_to[1] = max(Xscaled).
         Only returned if scale_from = None.
-
     scaled_from : (2,) tuple
         The minimum and maximum of the snapshot data, i.e., the bounds that
         the data was scaled from. Only returned if scale_from = None.
-
-    For scale_from=None, only Xscaled is returned.
 
     Examples
     --------
@@ -122,6 +113,265 @@ def scale(X, scale_to, scale_from=None):
     Xscaled = X*scl + (mini - xmin*scl)
 
     return (Xscaled, scale_to, scale_from) if learning else Xscaled
+
+
+class SnapshotTransformer:
+    """Process snapshots by scaling and/or centering."""
+    _VALID_SCALINGS = {
+        "standard",
+        "minmax",
+        "symminmax",
+        "maxabs",
+        "minmaxabs",
+    }
+
+    def __init__(self, scaling=None, center=False, verbose=0):
+        """Set transformation hyperparameters.
+
+        Parameters
+        ----------
+        scaling : str or None
+            If given, scale (non-dimensionalize) the snapshot data entrywise.
+            * 'standard': standardize to zero mean and unit standard deviation.
+            * 'minmax': minmax scaling to [0,1].
+            * 'symminmax': minmax scaling to [-1,1].
+            * 'maxabs': absolute maximum scaling to [-1,1] (no shift).
+            * 'minmaxabs': absolute min-max scaling to [-1,1] (mean shift).
+        center : bool
+            If True, shift the (scaled) snapshots by the mean snapshot.
+        verbose : int
+            Verbosity level. Options:
+            * 0 (default): no printouts.
+            * 1: some printouts.
+            * 2: lots of printouts.
+
+        Notes
+        -----
+        Standard scaling:
+            X' = (X - mean(X)) / std(X);
+            Guarantees mean(X') = 0, std(X') = 1.
+        Min-max scaling:
+            X' = (X - min(X))/(max(X) - min(X));
+            Guarantees min(X') = 0, max(X') = 1.
+        Symmetric min-max scaling:
+            X' = (X - min(X))*2/(max(X) - min(X)) - 1
+            Guarantees min(X') = -1, max(X') = 1.
+        Maximum absolute scaling:
+            X' = X / max(abs(X));
+            Guarantees mean(X') = mean(X) / max(abs(X)), max(abs(X')) = 1.
+        Min-max absolute scaling:
+            X' = (X - mean(X)) / max(abs(X - mean(X)));
+            Guarantees mean(X') = 0, max(abs(X')) = 1.
+        Centering:
+            X'' = X' - mean(X', axis=1);
+            Guarantees mean(X'', axis=1) = [0, ..., 0].
+        """
+        self.scaling = scaling
+        self.center = center
+        self.verbose = verbose
+
+    def _clear(self):
+        """Delete all learned attributes."""
+        for attr in ("scale_", "shift_", "mean_"):
+            if hasattr(self, attr):
+                delattr(self, attr)
+
+    # Properties --------------------------------------------------------------
+    @property
+    def scaling(self):
+        """Entrywise scaling (non-dimensionalization) directive.
+        * None: no scaling.
+        * 'standard': standardize to zero mean and unit standard deviation.
+        * 'minmax': minmax scaling to [0,1].
+        * 'symminmax': minmax scaling to [-1,1].
+        * 'maxabs': absolute maximum scaling to [-1,1] (no shift).
+        * 'minmaxabs': absolute min-max scaling to [-1,1] (mean shift).
+        """
+        return self.__scaling
+
+    @scaling.setter
+    def scaling(self, scl):
+        """Set the scaling strategy, checking for validity."""
+        if scl is None:
+            self._clear()
+            self.__scaling = scl
+            return
+        if not isinstance(scl, str):
+            raise TypeError("'scaling' must be of type 'str'")
+        if scl not in self._VALID_SCALINGS:
+            opts = ", ".join([f"'{v}'" for v in self._VALID_SCALINGS])
+            raise ValueError(f"invalid scaling '{scl}'; "
+                             f"valid options are {opts}")
+        self._clear()
+        self.__scaling = scl
+
+    # Reporting ---------------------------------------------------------------
+    @staticmethod
+    def _statistics_report(X):
+        """Return a string of basis statistics about a data set."""
+        return " | ".join([f"{f(X):<10.3e}"
+                           for f in (np.min, np.mean, np.max, np.std)])
+
+    # Persistence -------------------------------------------------------------
+    def save(self, filename, overwrite=False):
+        """Save the current transformer to an HDF5 file.
+
+        Parameters
+        ----------
+        filename : str
+            Path of the file to save the transformer in.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def load(cls, filename):
+        """Load a SnapshotTransformer from an HDF5 file.
+
+        Parameters
+        ----------
+        filename : str
+            Path to the file where the transformer was stored (via save()).
+
+        Returns
+        -------
+            SnapshotTransformer
+        """
+        raise NotImplementedError
+
+    # Main routines -----------------------------------------------------------
+    def fit_transform(self, X, inplace=False):
+        """Learn and apply the transformation.
+
+        Parameters
+        ----------
+        X : (n,k) ndarray
+            Matrix of k snapshots. Each column is a snapshot of dimension n.
+        inplace : bool
+            If True, overwrite the input data during transformation.
+            If False, create a copy of the data to transform.
+
+        Returns
+        -------
+        X'': (n,k) ndarray
+            Matrix of k transformed n-dimensional snapshots.
+        """
+        Y = X if inplace else X.copy()
+
+        # Scale (non-dimensionalize) each variable.
+        if self.scaling:
+            # Standard: X' = (X - µ)/σ
+            if self.scaling == "standard":
+                µ = np.mean(X)
+                σ = np.std(X)
+                self.scale_ = 1/σ
+                self.shift_ = -µ*self.scale_
+
+            # Min-max: X' = (X - min(X))/(max(X) - min(X))
+            elif self.scaling == "minmax":
+                Xmin = np.min(X)
+                Xmax = np.max(X)
+                self.scale_ = 1/(Xmax - Xmin)
+                self.shift_ = -Xmin*self.scale_
+
+            # Symmetric min-max: X' = (X - min(X))*2/(max(X) - min(X)) - 1
+            elif self.scaling == "symminmax":
+                Xmin = np.min(X)
+                Xmax = np.max(X)
+                self.scale_ = 2/(Xmax - Xmin)
+                self.shift_ = -Xmin*self.scale_ - 1
+
+            # MaxAbs: X' = X / max(abs(X))
+            elif self.scaling == "maxabs":
+                self.scale_ = 1/np.max(np.abs(X))
+                self.shift_ = 0
+
+            # MinMaxAbs: X' = (X - mean(X)) / max(abs(X - mean(X)))
+            elif self.scaling == "minmaxabs":
+                µ = np.mean(X)
+                Y -= µ
+                self.scale_ = 1/np.max(np.abs(Y))
+                self.shift_ = -µ*self.scale_
+                Y += µ
+
+            Y *= self.scale_
+            Y += self.shift_
+
+        # Center the scaled snapshots.
+        if self.center:
+            self.mean_ = np.mean(Y, axis=1)
+            Y -= self.mean_.reshape((-1,1))
+
+        if self.verbose >= 1:
+            if self.scaling is None and self.center is False:
+                report = ["No scaling learned!"]
+            else:
+                report = [f"Learned {self.scaling} scaling X -> X'"]
+            report.append("   |     min    |    mean    |     max    |    std")
+            sep = '-'*12
+            report.append(f"---|{sep}|{sep}|{sep}|{sep}")
+            report.append(f"X  | {self._statistics_report(X)}")
+            report.append(f"X' | {self._statistics_report(Y)}")
+            print('\n'.join(report))
+
+        return Y
+
+    def transform(self, X, inplace=False):
+        """Apply the learned transformation.
+
+        Parameters
+        ----------
+        X : (n,k) ndarray
+            Matrix of k snapshots. Each column is a snapshot of dimension n.
+        inplace : bool
+            If True, overwrite the input data during transformation.
+            If False, create a copy of the data to transform.
+
+        Returns
+        -------
+        X'': (n,k) ndarray
+            Matrix of k transformed n-dimensional snapshots.
+        """
+        Y = X if inplace else X.copy()
+
+        # Scale (non-dimensionalize) each variable.
+        if self.scaling:
+            Y *= self.scale_
+            Y += self.shift_
+
+        # Shift (center) the scaled snapshots.
+        if self.center:
+            Y -= self.mean_.reshape((-1,1))
+
+        return Y
+
+    def inverse_transform(self, X, inplace=False):
+        """Apply the inverse of the learned transformation.
+
+        Parameters
+        ----------
+        X : (n,k) ndarray
+            Matrix of k transformed n-dimensional snapshots.
+        inplace : bool
+            If True, overwrite the input data during inverse transformation.
+            If False, create a copy of the data to untransform.
+
+        Returns
+        -------
+        X'': (n,k) ndarray
+            Matrix of k untransformed n-dimensional snapshots.
+        """
+        Y = X if inplace else X.copy()
+
+        # Shift (uncenter) the scaled snapshots.
+        if self.center:
+            Y += self.mean_.reshape((-1,1))
+
+        # Unscale (re-dimensionalize) the unshifted snapshots.
+        if self.scaling:
+            Y -= self.shift_
+            Y /= self.scale_
+
+        return Y
 
 
 # Deprecations ================================================================
--- tests/pre/test_shift_scale.py
+++ tests/pre/test_shift_scale.py
@@ -2,6 +2,7 @@
 """Tests for rom_operator_inference.pre._shift_scale.py."""
 
 import pytest
+import itertools
 import numpy as np
 
 import rom_operator_inference as opinf
@@ -65,3 +66,147 @@ def test_scale(set_up_basis_data):
 
     # Verify inverse scaling.
     assert np.allclose(opinf.pre.scale(Xscaled, scaled_from, scaled_to), X)
+
+
+class TestSnapshotTransformer:
+    """Test pre.SnapshotTransformer."""
+    def test_init(self):
+        """Test pre.SnapshotTransformer.__init__()."""
+        st = opinf.pre.SnapshotTransformer()
+        for attr in ["scaling", "center", "verbose"]:
+            assert hasattr(st, attr)
+
+    def test_properties(self):
+        """Test pre.SnapshotTransformer properties (attribute protection)."""
+        st = opinf.pre.SnapshotTransformer()
+
+        # Test scale.
+        with pytest.raises(ValueError) as exc:
+            st.scaling = "minimaxii"
+        assert exc.value.args[0].startswith("invalid scaling 'minimaxii'")
+
+        with pytest.raises(TypeError) as exc:
+            st.scaling = [2, 1]
+        assert exc.value.args[0] == "'scaling' must be of type 'str'"
+
+        for s in st._VALID_SCALINGS:
+            st.scaling = s
+        st.scaling = None
+
+    def test_fit_transform(self, n=200, k=50):
+        """Test pre.SnapshotTransformer.fit_transform()."""
+
+        def fit_transform_copy(st, A):
+            """Assert A and B are not the same object but do have the same
+            type and shape.
+            """
+            B = st.fit_transform(A, inplace=False)
+            assert B is not A
+            assert type(B) is type(A)
+            assert B.shape == A.shape
+            return B
+
+        X = np.random.randint(0, 100, (n,k)).astype(float)
+        st = opinf.pre.SnapshotTransformer(scaling=None,
+                                           center=False, verbose=0)
+
+        # Test null transformation.
+        Y = st.fit_transform(X, inplace=True)
+        assert Y is X
+        X = np.random.randint(0, 100, (n,k)).astype(float)
+        Y = fit_transform_copy(st, X)
+        assert np.all(Y == X)
+
+        # Test standard scaling.
+        st.scaling = "standard"
+        Y = fit_transform_copy(st, X)
+        for attr in "scale_", "shift_":
+            assert hasattr(st, attr)
+            assert isinstance(getattr(st, attr), float)
+        assert np.isclose(np.mean(Y), 0)
+        assert np.isclose(np.std(Y), 1)
+
+        # Test min-max scaling.
+        st.scaling = "minmax"
+        Y = fit_transform_copy(st, X)
+        assert np.isclose(np.min(Y), 0)
+        assert np.isclose(np.max(Y), 1)
+
+        # Test min-max scaling.
+        st.scaling = "symminmax"
+        Y = fit_transform_copy(st, X)
+        assert np.isclose(np.min(Y), -1)
+        assert np.isclose(np.max(Y), 1)
+
+        # Test maximum absolute scaling.
+        st.scaling = "maxabs"
+        Y = fit_transform_copy(st, X)
+        assert np.isclose(np.max(np.abs(Y)), 1)
+
+        # Test minimum-maximum absolute scaling.
+        st.scaling = "minmaxabs"
+        Y = fit_transform_copy(st, X)
+        assert np.isclose(np.mean(Y), 0)
+        assert np.isclose(np.max(np.abs(Y)), 1)
+
+        # Test mean shift.
+        st.center = True
+        Y = fit_transform_copy(st, X)
+        assert hasattr(st, "mean_")
+        assert isinstance(st.mean_, np.ndarray)
+        assert st.mean_.shape == (X.shape[0],)
+        assert np.allclose(np.mean(Y, axis=1), 0)
+
+    def test_transform(self, n=200, k=50):
+        """Test pre.SnapshotTransformer.transform()."""
+        X = np.random.randint(0, 100, (n,k)).astype(float)
+        st = opinf.pre.SnapshotTransformer(scaling=None,
+                                           center=False, verbose=0)
+
+        # Test null transformation.
+        X = np.random.randint(0, 100, (n,k)).astype(float)
+        st.fit_transform(X)
+        Y = np.random.randint(0, 100, (n,k)).astype(float)
+        Z = st.transform(Y, inplace=True)
+        assert Z is Y
+        Y = np.random.randint(0, 100, (n,k)).astype(float)
+        Z = st.transform(Y, inplace=False)
+        assert Z is not Y
+        assert Z.shape == Y.shape
+        assert np.all(Z == Y)
+
+        # Test each scaling.
+        for scl in st._VALID_SCALINGS:
+            X = np.random.randint(0, 100, (n,k)).astype(float)
+            Y = np.random.randint(0, 100, (n,k)).astype(float)
+            st.scaling = scl
+            st.fit_transform(X)
+            a, b = st.scale_, st.shift_
+            Z = st.transform(Y)
+            assert np.allclose(Z, a*Y + b)
+
+        # Test mean shift.
+        st.center = True
+        st.scaling = None
+        X = np.random.randint(0, 100, (n,k)).astype(float)
+        Y = np.random.randint(0, 100, (n,k)).astype(float)
+        st.fit_transform(X)
+        µ = st.mean_
+        Z = st.transform(Y)
+        assert np.allclose(Z, Y - µ.reshape(-1,1))
+
+    def test_inverse_transform(self, n=200, k=50):
+        """Test pre.SnapshotTransformer.transform()."""
+        X = np.random.randint(0, 100, (n,k)).astype(float)
+        st = opinf.pre.SnapshotTransformer(scaling=None,
+                                           center=False, verbose=0)
+
+        for scaling, center in itertools.product({None, *st._VALID_SCALINGS},
+                                                 (True, False)):
+            st.scaling = scaling
+            st.center = center
+            st.fit_transform(X, inplace=False)
+            Y = np.random.randint(0, 100, (n,k)).astype(float)
+            Z = st.transform(Y, inplace=False)
+            st.inverse_transform(Z, inplace=True)
+            assert np.allclose(Z, Y)
